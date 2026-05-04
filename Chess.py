2099dd06492d4bbdf559839c784eb156577f081b
SeaMoose009 <seamusbailey97@gmail.com>
2026-05-04 10:44:21 +0930

    Update Chess.py
    Simplifying the code and adding comment to explain the function of each block of code. Additionally removing unnecessary features as this is the code in which the bots will be tested during it creation.

--- Chess.py
+++ Chess.py
@@ -1,60 +1,53 @@
 import chess
 import Bots
 
-# --------------------------------------------------------------#
+#--------------------------------------------------------------#
 
-def main(num_games, vs_state):
+def main():
 
     board = chess.Board()
 
-    print("Progress of games complete:")
-    for i in range(num_games):
-
-        board.reset()
-        move_list = []
+    board.reset()
+    move_list = []
 
         while board.is_checkmate() + board.can_claim_threefold_repetition() + board.is_insufficient_material() + board.can_claim_fifty_moves() + board.is_stalemate() == 0:
-                       #Player vs Bot
-            if vs_state == 1:
-                # 1
-                # White
-                print(f"{board}\n\n")
+# White
+# Displaying the current board state for the player to veiw
+            print(f"{board}\n\n")
 
-                can_move = True
-                Legal_List = []
 
-                for m in board.legal_moves:
-                    Legal_List.append(board.san(m))
+# Defining the valid move list that the player can make during their turn
+            Legal_List = []
 
-                print("Legal move list |>|", *Legal_List)
+            for m in board.legal_moves:
+                Legal_List.append(board.san(m))
 
-                while can_move == True:
-                    move = input("Make your move |>|")
+            print("Legal move list |>|", *Legal_List)
 
-                    try:
-                        can_move = False
-                        board.push_san(move)
-                        move_list.append(move)
-                    except chess.InvalidMoveError or chess.IllegalMoveError:
-                        can_move = True
-                        print("Invalid move type")
+            
+# Player input handling. Request input from user untill a valid response is made
+            can_move = True
+            while can_move == True:
+                move = input("Make your move |>|")
 
-                if board.is_checkmate() + board.can_claim_threefold_repetition() + board.is_insufficient_material() + board.can_claim_fifty_moves() + board.is_stalemate() == 0:
-                    # Black
-                    bot_move = Bots.bot_random(board)
-                    board.push_san(bot_move)
-                    move_list.append(bot_move)
+                try:
+                    can_move = False
+                    board.push_san(move)
+                    move_list.append(move)
+                except chess.InvalidMoveError or chess.IllegalMoveError:
+                    can_move = True
+                    print("Invalid move type")
 
-        print(f"{i + 1}/{num_games} || {round(((i + 1) / num_games) * 100)}%")
-    print("Simulation successfully executed")
+            if board.is_checkmate() + board.can_claim_threefold_repetition() + board.is_insufficient_material() + board.can_claim_fifty_moves() + board.is_stalemate() == 0:
+# Black
+# Requesting a move from the bot and making it
+                bot_move = Bots.bot_random(board)
+                board.push_san(bot_move)
+                move_list.append(bot_move)
+                
+# Prints the final board state and the moves of all pieces up untill that point once the game ends
     print(f"{board}\n\n")
     print(f"{move_list}\n\n")
 
 
-
-
-
-
-#First number represents the number of games
-#The second number being the type of gameplay. valid numbers being 0,1,2.
-main(1,0)
+main()
