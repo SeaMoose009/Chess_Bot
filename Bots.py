33212dfe42848bf6dd31c4837d88c0a4a7c4ecf0
SeaMoose009 <seamusbailey97@gmail.com>
2026-05-30 14:10:42 +0930

    Update Bots.py
    Tested the bot within Pycharm and made it properly Checkmate and avoid checkmate in one

--- Bots.py
+++ Bots.py
@@ -19,56 +19,56 @@ def bot_random(board):
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 # SACE ID number labeling the creator of the bot
-def bot_994625T(board, temperature=0): 
-# Creating a temperatry board where the future moves can be tested.
+def bot_994625T(board, temperature=0):
+    # Creating a temperatry board where the future moves can be tested.
     temp_board = chess.Board()
     board_fen = board.fen()
-# Defining the list of possible legal moves for the bots turn and the scoring
-# Moves will have the id of [Move, Score] (Move index = 0 and Score index = 1) <== maybe make it faster by setting to a uint_16
+    # Defining the list of possible legal moves for the bots turn and the scoring
+    # Moves will have the id of [Move, Score] (Move index = 0 and Score index = 1) <== maybe make it faster by setting to a uint_16
     Moves = []
-    
+
     for m in board.legal_moves:
-        Moves.append([board.san(m),0])
+        Moves.append([board.san(m), 0])
 
     if Moves.__len__() == 1:
         return Moves[0][0]
-        
-# -----------------------------------------------------------
-# Adding checkmate in one
-# ---------
-    
+
+    # -----------------------------------------------------------
+    # Adding checkmate in one
+    # ---------
+
     for i in Moves:
         if i[0][-1] == '#':
-            return i[0] # Immediately returning the move that checkmates to reduce runtime
-        
-# -----------------------------------------------------------
-# Advoiding checkmate in one
-# ---------
-        
+            return i[0]  # Immediately returning the move that checkmates to reduce runtime
+
+    # -----------------------------------------------------------
+    # Advoiding checkmate in one
+    # ---------
+
     for i in Moves:
         temp_board = chess.Board(board_fen)
-        temp_board.push(i[0])
+        temp_board.push_san(i[0])
         for m in temp_board.legal_moves:
-            if m[-1] == '#':
-            i[1] -= 10000 # Garentees that it avoids checkmate on itself
+            if temp_board.san(m)[-1] == '#':
+                i[1] -= 10000  # Garentees that it avoids checkmate on itself
 
+    # -----------------------------------------------------------
+    # Picking the best avaliable move within a given temperature
+    # ---------
 
-# -----------------------------------------------------------
-# Picking the best avaliable move within a given temperature
-# ---------
-    
     final_move_list = []
     best_score = 0
 
-    
-# First pass
+    # First pass
     for i in Moves:
-        if i[1] < best_score:
+        if i[1] > best_score:
             best_score = i[1]
-# Second pass
+    # Second pass
     for i in Moves:
-        if i[1] <= best_score - temperature:
+        if i[1] >= best_score - temperature:
             final_move_list.append(i[0])
-                
-# Returning a random move from the best options
-    return final_move_list[random.randint(0,final_move_list.__len__()-1)]
+
+    # Returning a random move from the best options
+    print(Moves)
+    print(final_move_list)
+    return final_move_list[random.randint(0, final_move_list.__len__() - 1)]
